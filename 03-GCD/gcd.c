a8f46a5779df83550dbae0b5716875eebf9fdf4a
kameswar98 <34705826+kameswar98@users.noreply.github.com>
2019-06-06 11:21:26 +0530

    Create gcd.c

---
+++ 03-GCD/gcd.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int gcd(int m, int n){
+
+    //get smallest
+    int smallest;
+    if(n<m||n==m){
+        smallest=n;
+    }else{
+        smallest=m;
+    }
+    int g;
+    for(int i=1;i<=smallest;i++){
+        if(m%i==0 && n%i==0){
+            g=i;
+        }
+    }
+    return g;
+}
+
+int main(int argc, char const *argv[])
+{
+    int m,n;
+    printf("Enter the input values (m,n): ");
+    scanf("%d%d",&n,&m);
+    
+    printf("gcd : %d",gcd(m,n));
+    
+}
