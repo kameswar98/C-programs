68e19533f4823b3c3f203ae686ac06e3200a6c9f
kameswar98 <34705826+kameswar98@users.noreply.github.com>
2019-06-06 11:22:34 +0530

    Create 9.poii.c

---
+++ 08-Point of intersection/9.poii.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+float x1,x2,_y1,y2;
+float x3,x4,y3,y4;
+float m1,c1,m2,c2;
+
+void getPoints(){
+    printf("Enter for (x1,y1) : ");
+    scanf("%f%f",&x1,&_y1);
+    printf("Enter for (x2,y2) : ");
+    scanf("%f%f",&x2,&y2);
+}
+
+void calcSlope(){
+    
+}
+
+void main(){
+    getPoints();
+    calcSlope();
+
+}
